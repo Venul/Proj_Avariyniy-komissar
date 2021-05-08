bb23e84d1f119aa6056cc881b4e7dc0806c62ad0
Lunev Sergey <s.lunev88@gmail.com>
2021-05-08 18:42:19 +0300

    alpha version

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
 $(function() {
-	VK.Widgets.Group("vk_groups", {
-		mode: 3,
-		width: "280px",
-		height: "200px"
-	}, 201892547);
-
 	$(".scroll-link").click(function() {
 		var object = $(this).attr("data-scroll");
 		var destination = $(object).offset().top;
